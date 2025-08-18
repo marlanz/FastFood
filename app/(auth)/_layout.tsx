5eccd7968e4e0a56198702b272be8360248c2834
marlanz <phucanhdodang1211@gmail.com>
2025-08-18 23:53:15 +0700

    feat(ui): add Button and Input components

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,7 +1,16 @@
-import { Redirect, Slot } from "expo-router";
+import CustomInput from "@/components/CustomInput";
+import { images } from "@/constants";
+import { Redirect } from "expo-router";
 import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 
 const _Layout = () => {
   const isAuthenticated = false;
@@ -9,10 +18,37 @@ const _Layout = () => {
     return <Redirect href="/" />;
   }
   return (
-    <SafeAreaView>
-      <Text>Auth Layout</Text>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+          />
+        </View>
+        <CustomInput
+          placeholder="Enter your email"
+          value=""
+          onChangeText={(text) => {}}
+          label="Email"
+          keyboardType="email-address"
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.jsm.foodordering",
+  databaseId: "68a1f97800175abdd57b",
+  userCollectionId: "68a1f9ad00107e7ae096",
+};
